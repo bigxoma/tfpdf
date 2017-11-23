29316baaaf8dac7b4e37d54c6b9ef948a2d5359b
Alexey Telyupin <Alexey.Telyupin@ascom.com>
2017-11-23 15:35:00 +0300

    update fonts and .gitignore

---
+++ src/font/unifont/roboto-font-metrics.php
@@ -0,0 +1,18 @@
+<?php
+$name='Roboto-Regular';
+$type='TTF';
+$arr_descriptors=array (
+  'Ascent' => 928.0,
+  'Descent' => -244.0,
+  'CapHeight' => 711.0,
+  'Flags' => 4,
+  'FontBBox' => '[-479 -271 1170 1056]',
+  'ItalicAngle' => 0.0,
+  'StemV' => 87.0,
+  'MissingWidth' => 249.0,
+);
+$flt_underline_pos=-73;
+$flt_underline_thickness=49;
+$ttffile='C:\OSPanel\domains\back-end.loc/src/fonts/unifont/roboto.ttf';
+$originalsize=158604;
+$fontkey='roboto';
